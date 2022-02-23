3698a40a6199b8d11a9e888dbdfdf2c51ee11f33
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-23 17:28:21 +0100

    errore

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ let areaNumbers = document.querySelector('.area-numbers');
 for (let i = 1; i <= 100; i++) {
     if (i % 5 == 0 && i % 3 == 0) {
         areaNumbers.innerHTML += '<div class= "box both">fizzbuzz</div>'
-    }  else if (i % 5 == 0) {
-        areaNumbers.innerHTML += '<div class= "box five">buzz</div>'
-    } else if (i % 3 == 0) {
-        areaNumbers.innerHTML += '<div class= "box three">fizz</div>'
+    }  else if (i % 3 == 0) {
+        areaNumbers.innerHTML += '<div class= "box three">buzz</div>'
+    } else if (i % 5 == 0) {
+        areaNumbers.innerHTML += '<div class= "box five">fizz</div>'
     }  else {
     areaNumbers.innerHTML += `<div class= "box">${i}</div>`
     }

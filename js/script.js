54792a411230442230f6be6b002d3085163f6758
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-23 17:02:34 +0100

    modifica

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 let areaNumbers = document.querySelector('.area-numbers');
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
-        areaNumbers.innerHTML += '<div class= "box three">fizz</div>'
-    } else if (i % 5 == 0) {
-        areaNumbers.innerHTML += '<div class= "box five">buzz</div>'
-    } else if (i % 5 == 0 && i % 3 == 0) {
+    if (i % 5 == 0 && i % 3 == 0) {
         areaNumbers.innerHTML += '<div class= "box both">fizzbuzz</div>'
-    } else {
+    }  else if (i % 5 == 0) {
+        areaNumbers.innerHTML += '<div class= "box five">buzz</div>'
+    } else if (i % 3 == 0) {
+        areaNumbers.innerHTML += '<div class= "box three">fizz</div>'
+    }  else {
     areaNumbers.innerHTML += `<div class= "box">${i}</div>`
     }
-}
+};
+
+
+

6f77d71ef0ce8f93df31ce2f81ab6ef88f3c11c0
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-23 16:26:56 +0100

    fizz buzz aggiunti

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ let areaNumbers = document.querySelector('.area-numbers');
 
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0) {
-        areaNumbers.innerHTML += `<div class= "box three">${i}</div>`
+        areaNumbers.innerHTML += '<div class= "box three">fizz</div>'
     } else if (i % 5 == 0) {
-        areaNumbers.innerHTML += `<div class= "box five">${i}</div>`
+        areaNumbers.innerHTML += '<div class= "box five">buzz</div>'
     } else if (i % 5 == 0 && i % 3 == 0) {
-        areaNumbers.innerHTML += `<div class= "box both">${i}</div>`
+        areaNumbers.innerHTML += '<div class= "box both">fizzbuzz</div>'
     } else {
     areaNumbers.innerHTML += `<div class= "box">${i}</div>`
     }
